cdb97051e5829411a7b2a73d5060d680c6477fd7
romorin <romorin@zoho.com>
2016-06-16 17:13:11 -0400

    name change + better config + nested radio_home mkdir

--- radiosh.autocmplete.sh
+++ radiosh.autocmplete.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 
-source "./radio-cmd.cfg"
+if [ -r ~/.config/radiosh/config ]; then
+	source ~/.config/radiosh/config
+elif [ -r /etc/radiosh.cfg ]; then
+	source /etc/radiosh.cfg
+elif [ -r ./radiosh.cfg ]; then
+	source ./radiosh.cfg
+else
+	return 1
+fi
 
-_radio-cmd () 
+_radiosh ()
 {
 	local cur prev opts
 	COMPREPLY=()
 	cur="${COMP_WORDS[COMP_CWORD]}"
 	prev="${COMP_WORDS[COMP_CWORD-1]}"
 	opts="kill list mute pause play start stop volume"
-	
+
 	case "${prev}" in
 		"play" )
 			local last=$(echo -n "${cur}" | tail -c -1)
-			
+
 			local chanDir
-			
+
 			if [ "${last}" = '/' ]; then
 				chanDir="${cur}"
 			else
 				chanDir=$(dirname "${cur}")
 			fi
-			
+
 			local fullDir
-			
+
 			if [ "${chanDir}" != '.' ]; then
 				fullDir="${RADIO_HOME}${chanDir}"
 			else
@@ -31,11 +39,11 @@ _radio-cmd ()
 			fi
 
 			local files=$(find "${fullDir}" -maxdepth 1 -type d  ! -path "${fullDir}" -printf "%p/\n" | sed -e "s?${RADIO_HOME}??" &&
-				find "${fullDir}" -maxdepth 1 -type f -printf "%p \n" | sed -e "s?${RADIO_HOME}??" )		
+				find "${fullDir}" -maxdepth 1 -type f -printf "%p \n" | sed -e "s?${RADIO_HOME}??" )
 
 			COMPREPLY=( $(compgen -W "${files}" -- "${cur}") )
 			compopt -o nospace
-			
+
 			return 0;
 			;;
 	esac
@@ -43,4 +51,4 @@ _radio-cmd ()
 	COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
 }
 
-complete -F _radio-cmd -o filenames ./radio-cmd.sh
+complete -F _radiosh -o filenames ./radiosh
